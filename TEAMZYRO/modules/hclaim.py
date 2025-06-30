3abaaf169436cf831a33eb80233263de3377a436
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-06-30 18:18:26 +0530

    Update hclaim.py

--- TEAMZYRO/modules/hclaim.py
+++ TEAMZYRO/modules/hclaim.py
@@ -48,7 +48,7 @@ async def mclaim(_, message: t.Message):
     try:
         # Ensure the user is in the correct chat
         if str(message.chat.id) != chat:
-            join_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Here", url="https://t.me/+WaK_ZLKBKvU3YzI9")]])
+            join_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Here", url="https://t.me/PBX_CHAT")]])
             return await message.reply_text(
                 "🔔 ᴊᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʟᴀɪᴍ ʏᴏᴜʀ ᴅᴀɪʟʏ ᴄʜᴀʀᴀᴄᴛᴇʀ 🔔",
                 reply_markup=join_button
